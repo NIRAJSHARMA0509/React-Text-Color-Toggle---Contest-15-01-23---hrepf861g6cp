5e4002cf4dab3be1bbade28937c137f9c38cc4cb
NIRAJSHARMA0509 <120631030+NIRAJSHARMA0509@users.noreply.github.com>
2023-01-15 19:07:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,17 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
+
+  const [isActive,setisActive]=useState(true);
+
+const changecolor=(()=>{
+  setisActive(current => !current);
+})
 //code here 
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={isActive ? 'redColor' : 'blueColor'} >Newton School</p>
+      <button id='button' onClick={changecolor}>Change Style</button>
     </div>
   )
 }
